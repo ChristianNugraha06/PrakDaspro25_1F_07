80262d049be500641281c1fb06888fa146449b32
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-10-23 11:46:38 +0700

    P9_Percobaan1_PertanyaanNO4

--- P9/siakadFor07.java
+++ P9/siakadFor07.java
@@ -7,6 +7,7 @@ public class siakadFor07 {
         Scanner Christ = new Scanner(System.in);
         
         double nilai, tertinggi=0, terendah =100;
+        int lulus =0, tdk_Lulus=0;
 
         for (int i = 1; i<=10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke - "+i+":");
@@ -17,7 +18,14 @@ public class siakadFor07 {
             if (nilai<terendah) {
                 terendah=nilai;
             }
+            if (nilai>60) { 
+                lulus++;
+            }else{
+                tdk_Lulus++;
+            }
         }System.out.println("nilai tertinggi"+tertinggi);
         System.out.println("nilai terendah"+terendah);
+        System.out.println("Jumlah lulus : "+lulus);
+        System.out.println("Jumlah tidak lulus : "+tdk_Lulus);
     }
 }

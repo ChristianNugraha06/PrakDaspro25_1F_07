f62a4328d7f2b0250137dfdfd31acb756037ee1b
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-10-23 18:27:57 +0700

    P9_Percobaan2

---
+++ P9/siakadWhile07.java
@@ -0,0 +1,37 @@
+package P9;
+
+import java.util.Scanner;
+
+public class siakadWhile07 {
+    public static void main(String[] args) {
+        Scanner Christ = new Scanner(System.in);
+        int i=0, nilai=0, jml;
+        System.out.print("Masukkan jumlah mahasiswa :");
+        jml=Christ.nextInt();
+
+        while (i<jml) {
+            System.out.print("Masukkan nilai mahasiswa ke - " + (i+1) + ":");
+            nilai=Christ.nextInt();
+            if (nilai <0 || nilai >100) {
+                System.out.print("Nilai tidak valid. Masukkan lagi nilai yang valid!");
+                continue;
+            }
+            if (nilai>80&&nilai<=100) {
+                System.out.println("Nilai mahasiswa ke -" +(i+1)+" adalah A");
+            }else if (nilai>73&&nilai<=80) {
+                System.out.println("Nilai mahasiswa ke-" +(i+1)+" adalah B+");
+            }else if (nilai>65&&nilai<=73) {
+                System.out.println("Nilai mahasiswa ke-" +(i+1)+" adalah B");
+            }else if (nilai>60&&nilai<=65) {
+                System.out.println("Nilai mahasiswa ke-" +(i+1)+" adalah C+");
+            }else if (nilai>50&&nilai<=60) {
+                System.out.println("Nilai mahasiswa ke-" +(i+1)+" adalah C");
+            }else if (nilai>39&&nilai<=50) {
+                System.out.println("Nilai mahasiswa ke-" +(i+1)+" adalah D");
+            }else{
+                System.out.println("Nilai mahasiswa ke-" +(i+1)+" adalah E");
+            }
+            i++;
+        }
+    }
+}

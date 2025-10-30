2307b1f1ebfc5058ad63210c421c3acccd21ec59
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-10-30 10:00:37 +0700

    P10_Percobaan2_PertanyaanNo2

--- P10/Square07.java
+++ P10/Square07.java
@@ -8,8 +8,8 @@ public class Square07 {
         System.out.print("Masukkan nilai n =");
         int n = Christ.nextInt();
 
-        for(int iOuter=0;iOuter<=n;iOuter++) {
-             for (int i=1;i<=n;i++ ) {
+        for(int iOuter=1;iOuter<=n;iOuter++) {
+             for (int i=0;i<=n;i++ ) {
                 System.out.print("*");
         }
         System.out.println();

666d62b14283787fc708a27dd39f66ba727ba60e
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-11-05 13:05:00 +0700

    P10_Tugas_No3

---
+++ P10/Caffe07.java
@@ -0,0 +1,39 @@
+package P10;
+
+import java.util.Scanner;
+
+public class Caffe07 {
+    public static void main(String[] args) {
+        Scanner Christ = new Scanner(System.in);
+
+        System.out.print("Jumlah cabang : ");
+        int Cabang = Christ.nextInt();
+
+        int totalPembeli = 0, totalBarang =0;
+
+        for (int i = 1; i <= Cabang; i++) {
+            System.out.println("--- Cabang " + i + "---" );
+            System.out.print("Jumlah pembeli: ");
+            int pembeli = Christ.nextInt();
+
+            int barangCabang =0;
+
+            for (int j = 1; j <=pembeli; j++) {
+                System.out.print("- Pembeli " + j + " Memesan berapa barang ");
+                int barang = Christ.nextInt();
+                barangCabang+=barang;
+            }
+            System.out.println(" Cabang " + i + ":");
+            System.out.println("Pembeli: " + pembeli + " Orang ");
+            System.out.println("Barang terjual: "+barangCabang);
+
+            totalPembeli+=pembeli;
+            totalBarang+=barangCabang;
+
+        }
+
+        System.out.println("Total seluruh cabang:");
+        System.out.println("Pembeli: "+totalPembeli);
+        System.out.println("Barang terjual "+totalBarang);
+    }
+}

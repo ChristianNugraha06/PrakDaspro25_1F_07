2be723249bb2185abeee0d16571f844e29c1b243
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-11-11 09:22:45 +0700

    P11_Percobaan2

---
+++ P11/ArrayNilai07.java
@@ -0,0 +1,26 @@
+package P11;
+
+import java.util.Scanner;
+
+public class ArrayNilai07 {
+    public static void main(String[] args) {
+        Scanner Christ = new Scanner(System.in);
+
+        int [] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir ke- " +i+" : ");
+            nilaiAkhir[i]=Christ.nextInt();
+            
+        } for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        }
+
+        
+           
+        
+
+    
+       
+    }
+}

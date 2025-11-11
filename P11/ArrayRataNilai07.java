f45614a19a6ad66d63542a56533a235fc4b54a67
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-11-11 15:38:22 +0700

    P11_Percobaan3_PertanyaanNo1

--- P11/ArrayRataNilai07.java
+++ P11/ArrayRataNilai07.java
@@ -9,13 +9,23 @@ public class ArrayRataNilai07 {
         int [] nilaiMhs= new int[10];
         double total =0;
         double rata2;
+         int mhsLulus =0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = Christ.nextInt();
-        }for (int i = 0; i < nilaiMhs.length; i++) {
-            total+=nilaiMhs[i];
         }
+        
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total+=nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                mhsLulus++;
+                
+                
+
+            } 
+            
+        } System.out.println("Total mahasiswa Lulus: " +mhsLulus);
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = "+rata2);
     }

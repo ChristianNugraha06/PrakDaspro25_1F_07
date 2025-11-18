c3018e5223a314841e348e6779d1202b4e30f190
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-11-18 20:51:31 +0700

    P12_Percobaan3_PertanyaanNo1

--- P12/numbers07.java
+++ P12/numbers07.java
@@ -10,6 +10,10 @@ public class numbers07 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
 
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
     
     }
 }

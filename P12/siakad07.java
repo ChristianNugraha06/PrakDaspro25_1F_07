b9b2396fb1ed6d0713df81c55694c9c3398fe960
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-11-18 21:24:13 +0700

    P12_Percobaan4

---
+++ P12/siakad07.java
@@ -0,0 +1,20 @@
+package P12;
+
+import java.util.Scanner;
+
+public class siakad07 {
+    public static void main(String[] args) {
+        Scanner Christ = new Scanner(System.in);
+
+        int [][] nilai =new int[4][3];
+
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Input nilai mahasiswa ke- " + (i + 1) );
+
+            for (int j = 0; j < nilai[i].length; j++) {
+                System.out.print("Nilai mata kuliah " + (j+1) +": ");
+                nilai[i][j] = Christ.nextInt();
+            }
+        }
+    }
+}

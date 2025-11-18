e81e4db79b73fa308f231de6f3b2d4b003fa80e6
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-11-18 20:48:35 +0700

    P12_Percobaan3(perbaikan)

--- P12/numbers07.java
+++ P12/numbers07.java
@@ -1,12 +1,15 @@
 package P12;
 
+import java.util.Arrays;
+
 public class numbers07 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][];
 
         myNumbers[0] = new int[5];
-        myNumbers[0] = new int[3];
-        myNumbers[0] = new int[1];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+
     
     }
 }

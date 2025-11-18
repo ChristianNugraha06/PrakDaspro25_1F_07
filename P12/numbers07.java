5045622d6fb2450695a4723cea514b30433adf2b
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-11-18 20:34:31 +0700

    P12_Percobaan3

---
+++ P12/numbers07.java
@@ -0,0 +1,12 @@
+package P12;
+
+public class numbers07 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][];
+
+        myNumbers[0] = new int[5];
+        myNumbers[0] = new int[3];
+        myNumbers[0] = new int[1];
+    
+    }
+}

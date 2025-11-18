0435c3c8d76e02e07ecb8602c1bcd5592f676cdc
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-11-18 21:38:21 +0700

    P12_Percobaan4_PenambahanDanModifikasiNo7dan8

--- P12/siakad07.java
+++ P12/siakad07.java
@@ -10,11 +10,28 @@ public class siakad07 {
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke- " + (i + 1) );
+            double totalPerSiswa=0;
 
             for (int j = 0; j < nilai[i].length; j++) {
                 System.out.print("Nilai mata kuliah " + (j+1) +": ");
                 nilai[i][j] = Christ.nextInt();
+                totalPerSiswa+=nilai[i][j];
             }
+
+            System.out.println("nilai rata-rata: " + totalPerSiswa/3);
+        }
+        System.out.println("\n======================================");
+        System.out.println("Rata rata nilai tiap matkul");
+
+        for (int j = 0; j < 3; j++) {
+            double totalperMatkul = 0;
+
+            for (int i = 0; i < 4; i++) {
+                totalperMatkul +=nilai[i][j];
+
+            }
+
+            System.out.println("Mata kuliah " + (j+1) + ": " + totalperMatkul/4);
         }
     }
 }

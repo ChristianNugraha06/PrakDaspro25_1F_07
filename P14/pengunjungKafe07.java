04bc6305c13aa94ddd43e8e00f7c0023970eacc0
ChristianNugraha06 <christiannugraha5@gmail.com>
2025-12-03 20:14:07 +0700

    P14_Percobaan4_PertanyaanNo2

--- P14/pengunjungKafe07.java
+++ P14/pengunjungKafe07.java
@@ -3,11 +3,13 @@ package P14;
 public class pengunjungKafe07 {
     static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar nama pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " +namaPengunjung[i]);
+       for (String nama : namaPengunjung) {
+        System.out.println("- "+ nama);
+        
+       }
 
           
-        }
+        
 
     } public static void main(String[] args) {
         daftarPengunjung("Ali","Budi","Citra");
